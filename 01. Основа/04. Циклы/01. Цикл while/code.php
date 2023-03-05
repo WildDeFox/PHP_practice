4a8e99f4d668f7fac0037aabdedec05d7b23eda4
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-06 02:10:14 +0700

    Цикл while

---
+++ 01. Основа/04. Циклы/01. Цикл while/code.php
@@ -0,0 +1,35 @@
+<?php
+// Выведите на экран числа от 1 до 100.
+$count1 = 1;
+while ($count1 <= 100) {
+    echo $count1 . PHP_EOL;
+    $count1++;
+}
+
+// Выведите на экран числа 11 до 33.
+$count2 = 11;
+while ($count2 <= 33) {
+    echo $count2 . PHP_EOL;
+    $count2++;
+}
+
+// Выведите на экран четные числа в промежутке от 0 до 100.
+$count3 = 0;
+while ($count3 <= 100) {
+    echo $count3 . PHP_EOL;
+    $count3 += 2;
+}
+
+// Выведите на экран нечетные числа в промежутке от 1 до 99.
+$count4 = 1;
+while ($count4 <= 99) {
+    echo $count4 . PHP_EOL;
+    $count4 += 2;
+}
+
+// Выведите на экран числа от 30 до 0.
+$count5 = 30;
+while ($count5 >= 0) {
+    echo $count5 . PHP_EOL;
+    $count5--;
+}
