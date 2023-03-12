1ce9f447691ed810f13ccba072c5c292e8dd9fad
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-12 20:09:52 +0700

    Скрытие формы после отправки

--- 01. Основа/09. Формы/03. Обработка формы в одном файле/code.php
+++ 01. Основа/09. Формы/03. Обработка формы в одном файле/code.php
@@ -1,11 +1,11 @@
+<?php if (empty($_POST)) { ?>
 <form action="" method="post">
     <input type="text" name="name"> Введите ваше Имя.<br>
     <input type="text" name="surn"> Введите вашу Фамилию.<br>
     <input type="text" name="patron"> Введите ваше Отчество.<br>
     <input type="submit">
 </form>
-
 <?php
-if (!empty($_POST)) {
+} else {
     echo "$_POST[name] $_POST[surn] $_POST[patron]";
 }
