2c9c19d523dd2340cc0f259cfa73707c611af1e0
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-12 19:18:36 +0700

    Условия и разрыв  кода

---
+++ 01. Основа/08. Формирование строк/07. Условия и разрыв PHP кода/code.php
@@ -0,0 +1,15 @@
+<?php $show = true; ?>
+
+<?php if($show): ?>
+<div>
+    <p>text+</p>
+    <p>text+</p>
+    <p>text+</p>
+</div>
+<?php else: ?>
+<div>
+    <p>text-</p>
+    <p>text-</p>
+    <p>text-</p>
+</div>
+<?php endif;?>
