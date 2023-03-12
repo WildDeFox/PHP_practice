751ff88a74d7f904c401b96f5264a6cb15c1dbe9
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-12 18:22:24 +0700

    Вставка переменных

---
+++ 01. Основа/08. Формирование строк/01. Вставка переменных/code.php
@@ -0,0 +1,15 @@
+<?php
+// $name = 'user';
+//	echo 'hello, ' . $name . '!';
+$name = 'user';
+echo "Hello $name <br>";
+
+//	$arr = ['1', '2', '3'];
+//	echo 'aaa ' . $arr[0] . ' bbb ' . $arr[1];
+$arr = ['1', '2', '3'];
+echo "aaa $arr[0] bbb $arr[1] <br>";
+
+//	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+//	echo 'text ' . $arr['a'] . ' text ' . $arr['b'] . ' text';
+$arr1 = ['a' => 1, 'b' => 2, 'c' => 3];
+echo "text $arr1[a] text $arr1[b] text";
