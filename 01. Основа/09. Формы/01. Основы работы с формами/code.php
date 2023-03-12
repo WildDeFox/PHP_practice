52747e260472fd50ab9980baec0a29d0d2819c3b
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-12 19:38:33 +0700

    Основы работы с формами

---
+++ 01. Основа/09. Формы/01. Основы работы с формами/code.php
@@ -0,0 +1,7 @@
+<?php // Сделайте форму с тремя инпутами, в которые можно ввести имя, возраст и зарплату пользователя. ?>
+<form action="result.php" method="post">
+    <input type="text" name="name">
+    <input type="number" name="age">
+    <input type="number" name="salary">
+    <input type="submit">
+</form>
