9011d4d95279e1223e5d68c55e22720721055c26
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-13 14:03:01 +0700

    Удаление сессий

--- 01. Основа/10. Сеанс/02. Использование сессий на одной странице/code.php
+++ 01. Основа/10. Сеанс/02. Использование сессий на одной странице/code.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
 
+if ($_SESSION['count'] === 10) {
+    unset($_SESSION['count']);
+}
+
 if(!isset($_SESSION['count'])) {
     $_SESSION['count'] = 1; // первый заход на страницу
 } else {
     $_SESSION['count']++; // последующие заходы
 }
+
+// Модифицируйте этот код так, чтобы при достижении счетчиком значения 10, отсчет начинался сначала.
+
 
 echo $_SESSION['count'];
