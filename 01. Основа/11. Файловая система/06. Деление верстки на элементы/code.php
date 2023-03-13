621de39ef500ded5a17a032320b493bdfa91740b
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-13 17:37:04 +0700

    Деление верстки на элементы

---
+++ 01. Основа/11. Файловая система/06. Деление верстки на элементы/code.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>title</title>
+</head>
+<body>
+<?php include 'header.php'; ?>
+<?php include 'aside.php'; ?>
+<main>
+    content
+</main>
+<?php include 'footer.php'; ?>
+</body>
+</html>
