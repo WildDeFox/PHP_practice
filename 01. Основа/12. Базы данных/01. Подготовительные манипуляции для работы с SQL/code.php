ea7451a7d103d32672cac6aca3c0ea2d3c10e975
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-15 22:35:51 +0700

    Подготовительные манипуляции для работы с

---
+++ 01. Основа/12. Базы данных/01. Подготовительные манипуляции для работы с SQL/code.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost'; // имя хоста
+$user = 'root'; // имя пользователя
+$pass = ''; // пароль
+$name = 'mydb'; // имя базы данных
+
+$link = mysqli_connect($host, $user, $pass, $name);
+
+$query = 'SELECT * FROM users';
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+var_dump($result);
