4a75ba644fe2d9b3392796b5ee1d3614c06d89dc
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-16 16:47:32 +0700

    Проблемы с кодировками

--- 01. Основа/12. Базы данных/01. Подготовительные манипуляции для работы с SQL/code.php
+++ 01. Основа/12. Базы данных/01. Подготовительные манипуляции для работы с SQL/code.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8">
 <?php
 $host = 'localhost'; // имя хоста
 $user = 'root'; // имя пользователя
@@ -5,6 +6,7 @@ $pass = ''; // пароль
 $name = 'mydb'; // имя базы данных
 
 $link = mysqli_connect($host, $user, $pass, $name);
+mysqli_query($link, "SET NAMES 'utf8'");
 
 $query = 'SELECT * FROM users';
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
