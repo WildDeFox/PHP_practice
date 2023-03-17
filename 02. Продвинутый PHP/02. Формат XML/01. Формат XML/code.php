8c6da4a327810a6199ed518a2cbcf73c9a1a6704
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-17 19:26:06 +0700

    Текст XML тега

--- 02. Продвинутый PHP/02. Формат XML/01. Формат XML/code.php
+++ 02. Продвинутый PHP/02. Формат XML/01. Формат XML/code.php
@@ -1,3 +1,2 @@
 <?php
 $xml = simplexml_load_file('test.xml');
-var_dump($xml);
---
+++ 02. Продвинутый PHP/02. Формат XML/02. Текст XML тега/code.php
@@ -0,0 +1,4 @@
+<?php
+$xml = simplexml_load_file('test.xml');
+echo $xml -> name . '<br>';
+echo $xml -> surn . '<br>';
