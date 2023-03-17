be425dc910421ae9824851c725012880c8872ffe
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-17 14:53:23 +0700

    Цепочка связанных таблиц

---
+++ 01. Основа/13. Организация БД/03. Цепочка связанных таблиц/code.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<?php
+$host = 'localhost'; // имя хоста
+$user = 'root'; // имя пользователя
+$pass = ''; // пароль
+$name = 'mydb'; // имя базы данных
+
+$link = mysqli_connect($host, $user, $pass, $name);
+mysqli_query($link, "SET NAMES 'utf8'");
+
+$query = "SELECT
+            users.name,
+            cities.name as city_name,
+            countries.name as country_name
+          FROM
+            users
+          LEFT JOIN cities ON cities.id=users.city_id
+          LEFT JOIN countries ON countries.id=cities.country_id";
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+
+// С помощью описанного цикла получите и выведите через var_dump на экран массив всех работников.
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row) {
+    var_dump($data);
+}
+echo '<br>';
