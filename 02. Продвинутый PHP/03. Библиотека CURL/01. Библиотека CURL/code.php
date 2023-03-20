b9aaba3e25fa599cf74ceb4cf3094cc8fceebf38
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-20 23:08:50 +0700

    Проверка ответа  на ошибку

--- 02. Продвинутый PHP/03. Библиотека CURL/01. Библиотека CURL/code.php
+++ 02. Продвинутый PHP/03. Библиотека CURL/01. Библиотека CURL/code.php
@@ -1,6 +1,6 @@
 <?php
 // Адрес страницы для обращения:
-$url = 'http://test.loc';
+$url = 'http://practice/02.%20Продвинутый%20PHP/03.%20Библиотека%20CURL/01.%20Библиотека%20CURL/test.loc';
 
 // Инициализируем сеанс:
 $curl = curl_init();
@@ -8,5 +8,15 @@ $curl = curl_init();
 // Указываем адрес страницы
 curl_setopt($curl, CURLOPT_URL, $url);
 
+// Ответ сервера сохранять в переменную
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
 // Выполняем запрос:
-curl_exec($curl);
+$res = curl_exec($curl);
+
+if ($res === false) {
+    // Выведем ошибку
+    echo curl_error($curl);
+} else {
+    var_dump($res);
+}
