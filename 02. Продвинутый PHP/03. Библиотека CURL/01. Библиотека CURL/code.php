dc27edee36ed236a85570d0099f0a0fe82a18389
WildMrDeFold <wildmrdefox@gmail.com>
2023-03-20 23:13:03 +0700

    Имитация браузера

--- 02. Продвинутый PHP/03. Библиотека CURL/01. Библиотека CURL/code.php
+++ 02. Продвинутый PHP/03. Библиотека CURL/01. Библиотека CURL/code.php
@@ -16,6 +16,8 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 // 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 //	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 
+// 	curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)');
+
 // Выполняем запрос:
 $res = curl_exec($curl);
 
